8a9d0fbe25ebc371203a6552503edb2369416223
Adrian Romero <romeroaadrian@gmail.com>
2020-02-11 19:01:20 -0500

    first commit

---
+++ employee.js
@@ -0,0 +1,27 @@
+class Employee {
+
+    constructor(name, id, title, email) {
+        this.name = name
+        this.id = id
+        this.title = title
+        this.email = email
+    }
+
+    getName(){
+        return this.name
+    }
+
+    getID(){
+        return this.id
+    }
+
+    getEmail(){
+        return this.email
+    }
+
+    getRole(){
+        return "Employee"
+    }
+}
+
+module.exports = Employee
